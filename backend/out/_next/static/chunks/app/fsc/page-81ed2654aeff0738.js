144d319e5a2fadeceba896904a91f49f0cade8f3
MRamzanPaz <m.ramzan@paz.agency>
2026-02-26 23:09:40 +0500

    chore(backend): sync backend-only project files

--- backend/out/_next/static/chunks/app/fsc/page-81ed2654aeff0738.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[919,164,991,311],{1268:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=1268)}),_N_E=n.O()}]);
